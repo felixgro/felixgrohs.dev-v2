2428952d46b951fc0ba250742272aa27325b0b81
felixgro <me@felixgrohs.dev>
2021-12-15 20:00:45 +0100

    fix(useWindowSize): remove window

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -8,21 +8,21 @@ interface WindowSize {
 
 const useWindowSize = (): WindowSize => {
     const [size, setSize] = useState({
-        width: window.innerWidth,
-        height: window.innerHeight,
+        width: window?.innerWidth || 0,
+        height: window?.innerHeight || 0,
     });
 
     const [handleResize] = useDebouncedCallback(() => {
         setSize({
-            width: window.innerWidth,
-            height: window.innerHeight,
+            width: window?.innerWidth || 0,
+            height: window?.innerHeight || 0,
         });
     }, 120, []);
 
     useEffect(() => {
-        window.addEventListener('resize', handleResize);
+        window?.addEventListener('resize', handleResize);
 
-        return () => window.removeEventListener('resize', handleResize);
+        return () => window?.removeEventListener('resize', handleResize);
     }, []);
 
     return size;
