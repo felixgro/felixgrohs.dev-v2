9e2a53987c9eefbad8ee050e3cc4744ee8bd4fbd
felixgro <me@felixgrohs.dev>
2021-12-22 21:57:45 +0100

    refactor(Project)

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -2,12 +2,13 @@ import { FunctionalComponent, h } from 'preact';
 import { useState, useMemo, useEffect } from 'preact/hooks';
 import useServerlessRequest from '@/hooks/useServerlessRequest';
 import ProjectTicker, { TickerState } from './ProjectTicker';
+import { Project as ProjectType } from './ProjectItem';
 import ProjectTickerDebugger from './ProjectTickerDebugger';
 import ProjectDialog from './ProjectDialog';
-import { Project as ProjectType } from './ProjectItem';
+import TabFocusTarget from '../shared/TabFocusTarget';
+import ProjectFallback from './ProjectFallback';
 import style from '#/Project.css';
 import stylePI from '#/ProjectItem.css';
-import TabFocusTarget from '../shared/TabFocusTarget';
 
 const Project: FunctionalComponent = () => {
 	const response = useServerlessRequest<ProjectType[]>('getProjects');
@@ -30,54 +31,49 @@ const Project: FunctionalComponent = () => {
 		return () => setState('idle');
 	}, []);
 
-	if (!projects /* !projects */) {
-		return (
-			<div class={style.projectWrapper}>
-				<small>Loading..</small>
-			</div>
-		);
-	} else {
-		return (
-			<TabFocusTarget
-				onTrigger={() => {
-					console.log('hi');
-				}}
-			>
-				<div class={style.projectWrapper}>
-					<ProjectDialog project={selectedProject} />
-					<ProjectTickerDebugger state={state} />
-					<div class={style.projects}>
-						<ProjectTicker
-							state={state}
-							projects={projects}
-							onProjectClicked={(proj) => {
-								setState('centering');
-
-								window.requestAnimationFrame(() => {
-									const prevSelected = document.querySelectorAll(
-										`.${stylePI.projectItemActive}`
-									);
-									const newSelected = document.querySelectorAll(
-										`[data-project-id='${proj.id}']`
-									);
-									prevSelected?.forEach((el) =>
-										el.classList.remove(stylePI.projectItemActive)
-									);
-									newSelected?.forEach((el) =>
-										el.classList.add(stylePI.projectItemActive)
-									);
-								});
-							}}
-							onProjectCentered={(proj, el) => {
-								setState('paused');
-								setSelectedProjectId(proj.id);
-							}}
-						/>
-					</div>
-				</div>
-			</TabFocusTarget>
-		);
+	if (!projects) {
+		return <ProjectFallback />;
 	}
+
+	return (
+		<TabFocusTarget
+			onTrigger={() => {
+				console.log('hi');
+			}}
+		>
+			<div class={style.projectWrapper}>
+				<ProjectDialog project={selectedProject} />
+				<ProjectTickerDebugger state={state} />
+				<div class={style.projects}>
+					<ProjectTicker
+						state={state}
+						projects={projects}
+						onProjectClicked={(proj) => {
+							setState('centering');
+							requestAnimationFrame(() => {
+								const prevSelected = document.querySelectorAll(
+									`.${stylePI.projectItemActive}`
+								);
+								const newSelected = document.querySelectorAll(
+									`[data-project-id='${proj.id}']`
+								);
+								prevSelected?.forEach((el) =>
+									el.classList.remove(stylePI.projectItemActive)
+								);
+								newSelected?.forEach((el) =>
+									el.classList.add(stylePI.projectItemActive)
+								);
+							});
+						}}
+						onProjectCentered={(proj) => {
+							setState('paused');
+							setSelectedProjectId(proj.id);
+						}}
+					/>
+				</div>
+			</div>
+		</TabFocusTarget>
+	);
 };
 
 export default Project;
